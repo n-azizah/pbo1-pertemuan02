a325fb90b2b228e2ea4a6dc5152c3b054227d8e6
nur.azizah <azizahn346@gmail.com>
2020-03-27 13:29:39 +0800

    Buat Output

--- model/InputOutput.java
+++ model/InputOutput.java
@@ -4,5 +4,9 @@ public class InputOutput{
  public static void main(String [] args){
   Scanner scanner = new Scanner(System.in);
 
+  String nama;
+  System.out.print("Masukan nama anda:  ");
+  nama  = scanner.nextLine();
+  System.out.println("Hai,  "+nama);
  }
 }
