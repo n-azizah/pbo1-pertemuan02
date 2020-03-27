1a6696cf15988c599e5c06860aedd0f375c22d55
nur.azizah <azizahn346@gmail.com>
2020-03-27 13:39:32 +0800

    Update InputOutput.java

--- model/InputOutput.java
+++ model/InputOutput.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class InputOutput{
  public static void main(String [] args){
   Scanner scanner = new Scanner(System.in);
-
   String nama;
   int jumlalhSks;
   double IPK;
