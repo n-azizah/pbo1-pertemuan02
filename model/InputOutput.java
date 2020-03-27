aba747f2c8eaf894572ed948caf0f0a14baf26f8
nur.azizah <azizahn346@gmail.com>
2020-03-27 14:09:17 +0800

    modifikasi file input

--- model/InputOutput.java
+++ model/InputOutput.java
@@ -4,7 +4,7 @@ public class InputOutput{
  public static void main(String [] args){
   Scanner scanner = new Scanner(System.in);
   String nama;
-  int jumlalhSks;
+  int jumlahSks;
   double IPK;
   
   System.out.print("Masukan nama anda:  ");
@@ -13,14 +13,11 @@ public class InputOutput{
 
   System.out.print("Masukan jumlah SKS yang sudah ditempuh: ");
   jumlahSks = scanner.nextInt();
-  System.out.println("Masukan jumlah SKS yang sudah ditempuh: "+jumlahSks);
 
   System.out.print("Masukan IPK Sementara :");
   IPK = scanner.nextDouble();
-  System.out.println("Masukan IPK Sementara : "+IPK);
 
-  doublejumlah = IPK * jumlahSks;
   System.out.print("Total Nilai = "+jumlah);
-  System.out.println("Total Nilai = "+doublejumlah);
+  double jumlah = IPK * jumlahSks;
  }
 }
