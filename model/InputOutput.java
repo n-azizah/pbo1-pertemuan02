6b0d1dd2efabaf3e3eb7569c0051677442b2e49b
nur.azizah <azizahn346@gmail.com>
2020-03-27 13:39:49 +0800

    Revert "Update InputOutput.java"
    This reverts commit 1a6696cf15988c599e5c06860aedd0f375c22d55.

--- model/InputOutput.java
+++ model/InputOutput.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class InputOutput{
  public static void main(String [] args){
   Scanner scanner = new Scanner(System.in);
+
   String nama;
   int jumlalhSks;
   double IPK;
