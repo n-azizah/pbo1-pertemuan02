af98fbc6ad0bbaf3cd8f61319f2633d1b4239d3e
nur.azizah <azizahn346@gmail.com>
2020-03-27 13:25:35 +0800

    Update InputOutput.java

--- model/InputOutput.java
+++ model/InputOutput.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class InputOutput{
- public class void main(String [] args){
- 
+ public static void main(String [] args){
+  Scanner scanner = new Scanner(System.in);
+
  }
 }
