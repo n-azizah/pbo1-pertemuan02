da5c4602a117a17fe041b514d892f66752e1b9b1
n-azizah <azizahn346@gmail.com>
2020-03-27 13:12:03 +0800

    Memanggil method dari banyak Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("== Data Mahasiswa========================");
+  System.out.println("NIDN  : 18630086");
+  System.out.println("Nama  : Nur Azizah Rizali");
+  System.out.println("=========================================");
+ }
+}
